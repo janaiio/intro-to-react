ea641fca51ac24660662fc0c298e0291784cc0bb
unknown <janais1991@gmail.com>
2018-12-08 01:20:33 -0600

    still working on deleteTodo method

--- src/App.js
+++ src/App.js
@@ -7,11 +7,12 @@ class App extends Component {
     super(props);
     this.state = {
       todos: [
-        { description: 'Walk the cat', isCompleted: true },
-        { description: 'Throw the dishes away', isCompleted: false },
-        { description: 'Buy new dishes', isCompleted: false }
+        { description: 'Walk the cat', isCompleted: true, isDeleted: onClick.target.value },
+        { description: 'Throw the dishes away', isCompleted: false, isDeleted: onClick.target.value },
+        { description: 'Buy new dishes', isCompleted: false, isDeleted: onClick.target.value }
       ],
-      newTodoDescription: ''
+      newTodoDescription: '',
+      completedFilter: clickStatus === 'completed'
     };
   }
 
@@ -26,6 +27,12 @@ class App extends Component {
     this.setState({ todos: [...this.state.todos, newTodo], newTodoDescription: '' });
   }
 
+  deleteTodo (e) {
+    if (!this.state.deletedTodo) { return };
+    const filteredTodo = { description: this.state.deletedTodo };
+    this.setState({ todos: [this.state.todos, filteredTodo], deletedTodo: ''});
+  }
+
   toggleComplete(index) {
     const todos = this.state.todos.slice();
     const todo = todos[index];
@@ -33,23 +40,29 @@ class App extends Component {
     this.setState({ todos: todos });
   }
 
-  deleteTodo = (index, e) => {
-    const todos = this.state.todos.filter(todo => {
-      return todo.key !== key;
-    const filteredTodos = Object.assign([], this.state.todos);
-    this.setState({ filteredTodos: todos });
-    console.log('deleteTodo clicked');
-  }
+// The code below did not kick back any errors, but was also non-responsive.
+// This is because `isDeleted` is not a defined property in this.state.
+// Consider how this can be resolved by clickStatus in this.state.
+
+//  deleteTodo (index) {
+//    const todos = (!this.state.todos.filter((item) => item.isDeleted));
+//    const todo = todos[index];
+//    todo.isDeleted = todo.isDeleted ? false : true;
+//    if (todo.isDeleted === 'true') {
+//      return todos;
+//    };
+//    this.setState({ todos: todos });
+//  }
 
   render() {
     return (
       <div className="App">
         <ul>
           { this.state.todos.map( (todo, index) =>
-            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) } deleteTodo={ this.deleteTodo.bind(this, index) }/>
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted } toggleComplete={ () => this.toggleComplete(index) }/> //isDeleted={ todo.isDeleted }  deleteTodo={ () => this.deleteTodo(index) }/>
           )}
         </ul>
-        <form onSubmit={ (e) => this.handleSubmit(e) }>
+        <form onSubmit={ (e) => this.handleSubmit(e) } onClick={ (e) => this.deleteTodo(e) }>
           <input type="text" value={ this.state.newTodoDescription } onChange={ (e) => this.handleChange(e) }/>
           <input type="submit" />
         </form>
